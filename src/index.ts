6e828bf83f3016d36daa1db95ba77346e5374642
kemalkaplan <kemal.kaplan@computatus.com>
2023-10-25 21:53:18 +0300

    test fix

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ export class TamedChatServer {
     private webServer?: http.Server | https.Server;
     private readonly DEFAULT_PORT = 5000;
 
+    
     constructor(webServer?: http.Server | https.Server) {
         try {
 
@@ -40,5 +41,17 @@ export class TamedChatServer {
         } catch (err) {
             logger.error("Error during starting Tamed Chat!", err);
         }
+    }
+    
+    registerAuthProvider = (authProvider: (socket: any, payload: any) => Promise<any>) => {
+        ChatServer.registerAuthProvider(authProvider);
+    }
+
+    registerPastMessagesProvider =  (pastMessagesProvider: (socket: any, payload: any) => Promise<any[]>) => {
+        ChatServer.registerPastMessagesProvider(pastMessagesProvider);
+    }
+
+    registerPrivacyProvider =  (privacyProvider: (socket: any, payload: any) => Promise<boolean>) => {
+        ChatServer.registerPrivacyProvider(privacyProvider);
     }
 }
--- test/index.ts
+++ test/index.ts
@@ -1,8 +1,6 @@
-import express, { Express } from "express";
-import http from "http";
-import ChatServer from "../src/chatServer";
 import * as dotenv from "dotenv";
 import log4js from "log4js";
+import { TamedChatServer } from "../src";
 
 dotenv.config({ path: `test/.${process.env.NODE_ENV}.env` });
 
@@ -15,12 +13,6 @@ log4js.configure({
 });
 const logger = log4js.getLogger("TamedChatTest");
 
-const app: Express = express();
-app.use(express.static('test/public'));
-const server = http.createServer(app).listen({ port: process.env.TAMED_CHAT_HTTP_PORT, host: '0.0.0.0' },
-    () => { console.log("Server running at http://localhost:" + process.env.TAMED_CHAT_HTTP_PORT) });
-
-
 const tmpMessageMap = {};
 let hashCode = function (s) {
     var h = 0, l = s.length, i = 0;
@@ -30,17 +22,17 @@ let hashCode = function (s) {
     return h;
 };
 
-ChatServer.start(server);
-ChatServer.registerAuthProvider((socket, payload) => {
+const chatServer = new TamedChatServer() 
+chatServer.registerAuthProvider((socket, payload) => {
     return Promise.resolve({ id: payload.data.userId })
 });
-ChatServer.registerPrivacyProvider((socket, payload) => {
+chatServer.registerPrivacyProvider((socket, payload) => {
     const hash = hashCode(socket.userId) | hashCode(payload.data.to);
     tmpMessageMap[hash] = (tmpMessageMap[hash] || [])
     tmpMessageMap[hash].push({ ...payload.data, ts: Date.now() });
     return Promise.resolve(true);
 });
-ChatServer.registerPastMessagesProvider((socket, payload) => {
+chatServer.registerPastMessagesProvider((socket, payload) => {
     const hash = hashCode(socket.userId) | hashCode(payload.data.to);
     return Promise.resolve(tmpMessageMap[hash]);
 });
